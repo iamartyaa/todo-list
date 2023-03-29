d360fdcc5d09e3d6f98254d90f347f4958662a1a
Amartya Yadav <iamartyayadav@gmail.com>
2023-03-30 00:24:33 +0530

    created basic app

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -1,12 +1,25 @@
-const AddNote = () => {
+import { useState } from "react";
+
+const AddNote = ({ handleAddNote }) => {
+
+    const [noteText, setNodeText] = useState('');
+
+    const handleChange = (e) => {
+        setNodeText((noteText) => noteText = e.target.value);
+    }
+
+    const handleSaveClick = (e) => {
+        handleAddNote(noteText);
+        setNodeText('');
+    }
+
     return (
         <div className="note new">
-            <textarea rows='5' cols='8' placeholder="Type a New note...">
-
+            <textarea value={noteText} onChange={handleChange} rows='5' cols='8' placeholder="Type a New note...">
             </textarea>
             <div className="note-footer">
                 <small>200 Remaining</small>
-                <button className="save">Save</button>
+                <button onClick={handleSaveClick} className="save">Save</button>
             </div>
         </div>
     )
